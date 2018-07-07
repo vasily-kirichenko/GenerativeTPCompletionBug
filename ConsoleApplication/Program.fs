d94cee90a5950499fca464ca0954cf23facb306a
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2018-07-07 10:47:41 +0300

    init

---
+++ ConsoleApplication/Program.fs
@@ -0,0 +1,6 @@
+﻿module Program
+
+open LibraryWithTP
+
+let clusterPort = Some 1 |> Option.defaultValue Settings.Default.Level1.L    
+//let clusterPort = Settings.Default.Level1. 
---
+++ LibraryWithTP/Library1.fs
@@ -0,0 +1,11 @@
+﻿namespace LibraryWithTP
+  
+module Settings =
+    open System
+    open System.IO 
+    open FSharp.Configuration
+                  
+    type Settings = YamlConfig<"Settings.yaml", ReadOnly=true>
+    
+    let Default = Settings()
+
